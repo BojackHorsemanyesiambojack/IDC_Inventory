a3028cb603a314cb268f906ace74262fc8a1c11b
Bojack <bojackhor9090@gmail.com>
2024-05-14 22:48:10 -0600

    Ahora si conecta bien

--- Inventory.cs
+++ Inventory.cs
@@ -10,17 +10,19 @@ using System.Windows.Forms;
 using System.Configuration;
 using System.Data.SqlClient;
 using MySql.Data.MySqlClient;
+using System.Data.OleDb;
 
 namespace IDC_inventory
 {
     public partial class IDC_inventory : Form
     {
-        MySqlConnection connection_to_database = new MySqlConnection(
-            "datasource = 127.0.0.1;" +
-            " port = 3306;" +
-            " username = root;" +
-            " password = MITSURI;" +
-            " database = idc_tienda");
+        OleDbConnection conn = new OleDbConnection(
+            @"Provider=Microsoft.ACE.OLEDB.12.0;" +
+            "Data Source=\"C:\\Users\\LENOVO THINKPAD\\Documents\\IDC inventory\\IDC inventory\\DataBases\\Poo-Inventario.accdb\";" +
+            "Persist Security Info=True");
+
+        string actualTable;
+
         DataTable data_box = new DataTable();
 
         public IDC_inventory()
@@ -28,28 +30,21 @@ namespace IDC_inventory
             InitializeComponent();
         }
 
+        private void get_data_and_renderize (string table)
+        {
+                conn.Open();
+                string sqlGetCommand = $"SELECT Almacen.* FROM Almacen";
+                OleDbDataAdapter adapter = new OleDbDataAdapter(sqlGetCommand, conn);
+                adapter.Fill(data_box);
+                dataGridView1.DataSource = data_box;
+                conn.Close();
+
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            get_data_services();
-        }
-                
-        private void get_data_services()
-        {
-            MySqlDataAdapter get_from_database = new MySqlDataAdapter("SELECT * FROM camisetas", connection_to_database);
-            get_from_database.Fill(data_box);
-            this.dataGridView1.DataSource = data_box;
-            this.dataGridView1.ReadOnly = true;
-        }
-
-
-        private void insert_data_services(string arg1, string arg2,string arg3, string table = "")
-        {
-            connection_to_database.Open();
-            MySqlCommand insert_to_database = new MySqlCommand(
-                $"INSERT INTO {table}" +
-                $" VALUES ('{arg1}','{arg2}','{arg3}')", connection_to_database);
-            int insertFiles = insert_to_database.ExecuteNonQuery();
-            connection_to_database.Close();
+            actualTable = "Almacen";
+            get_data_and_renderize(actualTable);
         }
 
         private void show_insert_data_form ()
@@ -62,19 +57,8 @@ namespace IDC_inventory
             show_insert_data_form();
         }
 
-        private void clean_data_box()
-        {
-            data_box.Clear();
-            dataGridView1.DataSource = null;
-            dataGridView1.DataSource = connection_to_database;
-            formInsert.Visible = false;
-        }
-
         private void button2_Click(object sender, EventArgs e)
         {
-            insert_data_services(Color_input.Text, type_input.Text, cant_input.Text);
-            clean_data_box();
-            get_data_services();
 
         }
         //-----------------------------------------------------------------------------------------------------------//
